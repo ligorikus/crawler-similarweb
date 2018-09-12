685c61f3f48544627b273c30518f77e5d62c7f09
ligo <semal551@gmail.com>
2018-09-12 12:13:01 +0600

    pipeline to sites.json

--- similarweb/pipelines.py
+++ similarweb/pipelines.py
@@ -5,7 +5,22 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
+
 
 class SimilarwebPipeline(object):
+    def __init__(self):
+        self.file = open('sites.json', 'wb')
+
+    def open_spider(self, spider):
+        self.file.write(b"[\n")
+
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+
+        self.file.write(line.encode())
         return item
+
+    def close_spider(self, spider):
+        self.file.write(b"]")
