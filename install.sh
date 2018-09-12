a5dd7acfd45d16dcc0ae738f54adc60bf3777d0b
root <root@dev.minerscamp.ru>
2018-09-12 07:29:42 +0000

    change install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-sudo apt-get install python3.6 python3.6-dev python3-pip
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt update
+sudo apt-get install -y python3 python3-dev python3-pip
 pip3 install Scrapy
-scrapy crawl similar
+
