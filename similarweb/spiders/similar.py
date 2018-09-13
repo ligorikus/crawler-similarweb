4cb5edb69e2078df72c336e088c3c3d0491356f9
ligo <semal551@gmail.com>
2018-09-13 14:24:22 +0600

    headers

--- similarweb/spiders/similar.py
+++ similarweb/spiders/similar.py
@@ -10,11 +10,13 @@ class SimilarSpider(scrapy.Spider):
     allowed_domains = ['similarweb.com']
 
     def start_requests(self):
+        headers = dict()
+        headers['User-Agent'] = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
         default_url = 'https://www.similarweb.com/website/'
         domains_data = open('domains.json')
         domains = json.load(domains_data)
         for domain in domains:
-            yield scrapy.Request(default_url+domain, callback=self.parse)
+            yield scrapy.Request(default_url+domain, callback=self.parse, headers=headers)
 
     def parse(self, response):
         traffic_response = re.findall('WeeklyTrafficNumbers":{[^}]*}', response.text)[0]
@@ -25,6 +27,3 @@ class SimilarSpider(scrapy.Spider):
 
         item = SimilarwebItem(domain=domain, traffic=traffic)
         yield item
-
-
-
